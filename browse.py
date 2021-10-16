b4ad96dd4705e64e3b9a2f89b88630514d897e0b
yifeng <zhan6987@umn.edu>
2021-10-15 23:07:32 -0500

    init

---
+++ browse.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import json
+import os
+import cv2
+
+GQA_Path = "/home/yifeng/Datasets/GQA/images/images/"
+filenames = os.listdir(GQA_Path)
+filenames.sort()
+for filename in filenames[0:1000]:
+    path = GQA_Path + filename
+    img = cv2.imread(path, 0)
+    cv2.imshow(filename, img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
